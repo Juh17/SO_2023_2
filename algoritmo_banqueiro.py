5846f52f7d1f2684c46c861f3b11a18a42d61a7e
Juliana Lima <102184995+Juh17@users.noreply.github.com>
2023-12-06 12:51:17 -0300

    Update algoritmo_banqueiro.py

--- algoritmo_banqueiro.py
+++ algoritmo_banqueiro.py
@@ -1,5 +1,5 @@
 def main():
-    vetor_recursos_existentes = [5,4,2,1]
+    vetor_recursos_existentes = [4,3,2,1]
     vetor_recursos_disponiveis = [2,1,0,0]
 
     matriz_alocacao_corrente = [
@@ -8,55 +8,62 @@ def main():
         [0,1,2,0]
     ]
 
-    matriz_de_requisicoes = [
+    matriz_requisicoes = [
         [2,0,0,1],
         [1,0,1,0],
-        [2,1,0,1]
+        [2,1,0,0]
     ]
 
-    linha_recursos_p1 = matriz_alocacao_corrente[0]
-    linha_recursos_p2 = matriz_alocacao_corrente[1]
-    linha_recursos_p3 = matriz_alocacao_corrente[2]
+    recursos_p1 = matriz_alocacao_corrente[0]
+    recursos_p2 = matriz_alocacao_corrente[1]
+    recursos_p3 = matriz_alocacao_corrente[2]
 
-    linha_recursos_necessarios_p1 = matriz_de_requisicoes[0]
-    linha_recursos_necessarios_p2 = matriz_de_requisicoes[1]
-    linha_recursos_necessarios_p3 = matriz_de_requisicoes[2]
-    recursos = usar_e_liberar_recursos(vetor_recursos_disponiveis, linha_recursos_necessarios_p1, linha_recursos_necessarios_p2, linha_recursos_necessarios_p3)
-    verificar_impasse = verificar_igualdade_recursos(vetor_recursos_disponiveis, vetor_recursos_existentes)
+    recursos_necessarios_p1 = matriz_requisicoes[0]
+    recursos_necessarios_p2 = matriz_requisicoes[1]
+    recursos_necessarios_p3 = matriz_requisicoes[2]
+    recursos = usar_e_liberar(vetor_recursos_disponiveis, recursos_necessarios_p1, recursos_necessarios_p2, recursos_necessarios_p3)
+    verificar_impasse = igualdade_recursos(vetor_recursos_disponiveis, vetor_recursos_existentes)
 
 
-    print(f'Vetor de recursos existentes: {vetor_recursos_existentes}')
-    print(f'Vetor de recursos disponíveis: {vetor_recursos_disponiveis}')
+    print(f'Recursos existentes: {vetor_recursos_existentes}')
+    print(f'Recursos disponíveis: {vetor_recursos_disponiveis}')
     print(f'\nMatriz de alocação corrente: {matriz_alocacao_corrente}')
-    print(f'\nMatriz de requisições: {matriz_de_requisicoes}')
+    print(f'\nMatriz de requisições: {matriz_requisicoes}')
 
     
     print(verificar_impasse)
 
-def usar_e_liberar_recursos(disponiveis, requeridos_p1, requeridos_p2, requeridos_p3):
-    
+
+def usar_e_liberar(disponiveis, p1, p2, p3):
+    deadlock = True  
+    alocou_recursos = False  
+
     for i in range(len(disponiveis)):
-        if all(requeridos_p1) <= disponiveis[i]:
-            disponiveis[i] += requeridos_p1[i]     
-        if all(requeridos_p2) <= disponiveis[i]:
-            disponiveis[i] += requeridos_p2[i]
-            #print(vetor_recursos_disponiveis)     
-        if all(requeridos_p3) <= disponiveis[i]:
-            disponiveis[i] += requeridos_p3[i]
-        break
-    print('Há algum processo em deadlock/impasse')
+       
+        if all(p1) <= disponiveis[i]:
+            disponiveis[i] += p1[i]
+            alocou_recursos = True  
+            deadlock = False 
+        if all(p2) <= disponiveis[i]:
+            disponiveis[i] += p2[i]
+            alocou_recursos = True 
+            deadlock = False 
+        if all(p3) <= disponiveis[i]:
+            disponiveis[i] += p3[i]
+            alocou_recursos = True  
+            deadlock = False  
+
+    # Verifica se nenhum processo alocou recursos
+    if not alocou_recursos:
+        print('Todos os processos estão em deadlock/impasse')
 
     return disponiveis
-  
 
-
-def verificar_igualdade_recursos(disponiveis, existentes):
- 
+def igualdade_recursos(disponiveis, existentes):
     for recurso in range(len(disponiveis)):
-        if disponiveis[recurso] == existentes[recurso]:
-            return 'Todos os processos rodaram'
-        else:
+        if disponiveis[recurso] != existentes[recurso]:
             return 'Pode haver impasse/deadlock'
-        
+    return 'Todos os processos rodaram'
+
 
 main()
